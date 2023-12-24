0fe72d80cf1f783f5ee15befcf8dce151a8fea36
tomazcx <tomazcx06@gmail.com>
2023-12-23 23:44:59 -0300

    feat: initial commit

---
+++ lua/tomazcx/init.lua
@@ -0,0 +1,6 @@
+require("tomazcx.remap")
+require("tomazcx.packer")
+
+vim.opt.number = true
+vim.cmd.colorscheme "catppuccin"
+
---
+++ lua/tomazcx/packer.lua
@@ -0,0 +1,46 @@
+-- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
+-- Only required if you have packer configured as `opt`
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+	use 'feline-nvim/feline.nvim'
+
+	use 'm4xshen/autoclose.nvim'
+	use {"akinsho/toggleterm.nvim", tag = '*' }
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.5',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use { "catppuccin/nvim", as = "catppuccin", config = function()
+		vim.cmd("colorscheme catppuccin")
+	end}
+	use ( 'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'} )
+	use ( 'duane9/nvim-rg' )
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			'nvim-tree/nvim-web-devicons', -- optional
+		},
+	}
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v3.x',
+		requires = {
+			--- Uncomment these if you want to manage LSP servers from neovim
+			{'williamboman/mason.nvim'},
+			{'williamboman/mason-lspconfig.nvim'},
+
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},
+			{'hrsh7th/cmp-nvim-lsp'},
+			{'L3MON4D3/LuaSnip'},
+		}
+	}
+end)
